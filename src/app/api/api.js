1a5a691d06e6372b3e2ff5b4b01e441c72727a10
Adam Mornes <adammornes@gmail.com>
2020-09-08 03:13:32 -0500

    added api

---
+++ src/app/api/api.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const url = 'https://www.omdbapi.com/?apikey=586d0240';
+
+const movieIds = [
+    'tt0389790',
+    'tt1375666',
+    'tt0076759',
+    'tt0242423',
+    'tt5817168'
+];
+
+export default new Promise((resolve, reject) => {
+    Promise.all(
+        movieIds.map(id => axios.post(`${url}&i=${id}`)
+    )).then(values => {
+        resolve(values.map(response => response.data));
+    }).catch(err => {
+        reject(err);
+    });
+});
--- src/app/components/main.js
+++ src/app/components/main.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Movies from './movies/movies';
+import getMovies from '../api/api';
 
 export default () => {
+    useEffect(() => {
+        getMovies.then(values => {
+            console.log(values);
+        })
+    }, []);
+
     return(
-        <main className="flex-1"></main>
+        <main className="container mx-auto flex-1">
+            <Movies />
+        </main>
     )
 }
