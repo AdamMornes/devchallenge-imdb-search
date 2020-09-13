571d78911798628eccfb917a011f531ee7b8acce
Adam Mornes <adammornes@gmail.com>
2020-09-13 15:08:31 -0500

    added shell and basic component for movie and styling

--- src/app/components/main.js
+++ src/app/components/main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
 
 import Movies from './movies/movies';
+import Movie from './movies/movie/movie';
 import BestMovie from './best-movie/best-movie';
 import NotFound from './shared/not-found';
 
@@ -9,7 +10,8 @@ export default () => {
     return(
         <main className="container mx-auto">
             <Switch>
-                <Route path="/movies" component={Movies} />
+                <Route path="/movies" component={Movies} exact/>
+                <Route path="/movies/:id" component={Movie} />
                 <Route path="/best-movie" component={BestMovie} />
                 <Redirect from="/" to="/movies" exact/>
                 <Route component={NotFound} />
---
+++ src/app/components/movies/movie/movie.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+export default props => {
+    return(
+        <div className="grid grid-cols-3 gap-8 my-8 -mx-4">
+            <div>
+                <img
+                    className="w-full border-2 border-gray-300 p-1"
+                    src="https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg"
+                    alt="Inception" />
+            </div>
+
+            <div className="col-span-2">
+                <h1 className="text-3xl font-bold leading-tight mb-4">Inception</h1>
+
+                <p className="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae porttitor ante, at pharetra magna. Pellentesque eget nulla in elit elementum ultricies quis vel urna. Fusce purus lacus, sodales et convallis eu, euismod ac ipsum. In rutrum ex tincidunt, sodales leo vitae, commodo magna. Quisque gravida ante ipsum, ut ultrices diam convallis fermentum.</p>
+                    
+                <h2 className="font-bold text-lg">Ratings:</h2>
+
+                <hr className="my-1" />
+
+                <ul className="text-sm">
+                    <li>
+                        <span className="font-bold">Metascore:</span> 74
+                    </li>
+                    <li>
+                        <span className="font-bold">IMDB:</span> 8.8
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
